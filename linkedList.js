568bf49d8497b88f5117ce119a8ea79cc35a0441
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-10 11:54:19 -0400

    add comments

--- linkedList.js
+++ linkedList.js
@@ -15,13 +15,13 @@ class LinkedList {
 
   addToTail(data) {
     let tail = this.head;
-    if (!tail) {
-      this.head = new Node(data)
-    } else {
-      while (tail.getNextNode()) {
+    if (!tail) { // the list is empty
+      this.head = new Node(data); // create tail and head with data
+    } else { // the list is not empty
+      while (tail.getNextNode()) { // iterate the list until we find the end
         tail = tail.getNextNode();
       }
-      tail.setNextNode(new Node(data));
+      tail.setNextNode(new Node(data)); // when iteration is over, the tail equals a new node with data arg
     }
   }
 }
